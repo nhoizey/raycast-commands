2cc4bbe28f48afcb3899f303650ac592bfb5c009
Nicolas Hoizey <nhoizey@clever-age.com>
2021-03-19 09:57:40 +0100

    Enhance note with content

--- new-note.sh
+++ new-note.sh
@@ -2,11 +2,12 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Create a note for nicolas-hoizey.com
+# @raycast.title New note
 # @raycast.mode silent
 
 # Optional parameters:
 # @raycast.icon https://nicolas-hoizey.com/assets/me/nicolas-hoizey-180px.png
+# @raycast.argument1 { "type": "text", "placeholder": "Content", "optional": true }
 
 # Documentation:
 # @raycast.description Create a note for nicolas-hoizey.com
@@ -21,9 +22,6 @@ then
 else
   content="$1"
 fi
-
-# Urlencode
-content=$(php -r "echo urlencode(\"$content\");")
 
 cd /Users/nhoizey/Documents/Devs/@perso/nicolas-hoizey.com
 ./_scripts/new_note.sh "$content"
